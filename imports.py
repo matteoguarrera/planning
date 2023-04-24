16ca1a3a7a5a6c18bbee7ef49dc4aed87ea28a22
Margarita Geleta <noticiasmundiales99@gmail.com>
2023-04-23 17:41:10 -0700

    adapt comment

--- imports.py
+++ imports.py
@@ -34,8 +34,8 @@ from diffusers.optimization import get_scheduler
 from tqdm.auto import tqdm
 
 # env import
-import gym
-from gym import spaces
+#import gym
+#from gym import spaces
 
 # import gymnasium as gym
 # from gymnasium import spaces
@@ -44,16 +44,16 @@ import numpy as np
 
 from datetime import datetime
 
-import pygame
-import pymunk
-import pymunk.pygame_util
-from pymunk.space_debug_draw_options import SpaceDebugColor
-from pymunk.vec2d import Vec2d
-import shapely.geometry as sg
-import cv2
-import skimage.transform as st
-from skvideo.io import vwrite
-from IPython.display import Video
+#import pygame
+#import pymunk
+#import pymunk.pygame_util
+#from pymunk.space_debug_draw_options import SpaceDebugColor
+#from pymunk.vec2d import Vec2d
+#import shapely.geometry as sg
+#import cv2
+#import skimage.transform as st
+#from skvideo.io import vwrite
+#from IPython.display import Video
 import gdown
 import os
 
@@ -120,36 +120,36 @@ When False::
     
 """
         
-def light_color(color: SpaceDebugColor):
-    color = np.minimum(1.2 * np.float32([color.r, color.g, color.b, color.a]), np.float32([255]))
-    color = SpaceDebugColor(r=color[0], g=color[1], b=color[2], a=color[3])
-    return color
+# def light_color(color: SpaceDebugColor):
+#     color = np.minimum(1.2 * np.float32([color.r, color.g, color.b, color.a]), np.float32([255]))
+#     color = SpaceDebugColor(r=color[0], g=color[1], b=color[2], a=color[3])
+#     return color
 
 
 
-def to_pygame(p: Tuple[float, float], surface: pygame.Surface) -> Tuple[int, int]:
-    """Convenience method to convert pymunk coordinates to pygame surface
-    local coordinates.
+# def to_pygame(p: Tuple[float, float], surface: pygame.Surface) -> Tuple[int, int]:
+#     """Convenience method to convert pymunk coordinates to pygame surface
+#     local coordinates.
 
-    Note that in case positive_y_is_up is False, this function wont actually do
-    anything except converting the point to integers.
-    """
-    if positive_y_is_up:
-        return round(p[0]), surface.get_height() - round(p[1])
-    else:
-        return round(p[0]), round(p[1])
-
-
+#     Note that in case positive_y_is_up is False, this function wont actually do
+#     anything except converting the point to integers.
+#     """
+#     if positive_y_is_up:
+#         return round(p[0]), surface.get_height() - round(p[1])
+#     else:
+#         return round(p[0]), round(p[1])
 
 
-def pymunk_to_shapely(body, shapes):
-    geoms = list()
-    for shape in shapes:
-        if isinstance(shape, pymunk.shapes.Poly):
-            verts = [body.local_to_world(v) for v in shape.get_vertices()]
-            verts += [verts[0]]
-            geoms.append(sg.Polygon(verts))
-        else:
-            raise RuntimeError(f'Unsupported shape type {type(shape)}')
-    geom = sg.MultiPolygon(geoms)
-    return geom
+
+
+# def pymunk_to_shapely(body, shapes):
+#     geoms = list()
+#     for shape in shapes:
+#         if isinstance(shape, pymunk.shapes.Poly):
+#             verts = [body.local_to_world(v) for v in shape.get_vertices()]
+#             verts += [verts[0]]
+#             geoms.append(sg.Polygon(verts))
+#         else:
+#             raise RuntimeError(f'Unsupported shape type {type(shape)}')
+#     geom = sg.MultiPolygon(geoms)
+#     return geom
